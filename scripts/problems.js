17b4fae3a5614219ca703dacb6cd3e4e355cc219
eshelton328 <erik.shelton328@gmail.com>
2025-02-03 20:00:04 -0600

    Added rotate image

--- scripts/problems.js
+++ scripts/problems.js
@@ -576,6 +576,23 @@ export default {
                     }
                 ]
             }
+        },
+        math: {
+            RotateImage: {
+                type: "fn",
+                fn: "rotate",
+                args: "matrix: number[][]",
+                return: "void",
+                difficulty: "medium",
+                tags: ["array", "math"],
+                description: "You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise). You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. Do not allocate another 2D matrix and do the rotation.",
+                examples: [
+                    {
+                        args: "[[1, 2, 3], [4, 5, 6], [7, 8, 9]]",
+                        return: "[[7, 4, 1], [8, 5, 2], [9, 6, 3]]",
+                    }
+                ]
+            }
         }
     },
     algorithms: {
